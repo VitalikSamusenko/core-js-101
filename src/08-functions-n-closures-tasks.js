e895196687fd3f0a499a1b14ff804d6363c9c1fb
Vitalik Samusenko <vitaliksamusenko1997@gmail.com>
2022-07-25 15:06:11 +0300

    feat: complete last 08 task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -142,8 +142,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsS = args.map((item) => JSON.stringify(item)).join(',');
+    logFunc(`${func.name}(${argsS}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${argsS}) ends`);
+    return res;
+  };
 }
 
 
